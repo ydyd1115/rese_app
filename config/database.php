83fcdce0718199ea750b59d57df71a1a30b6b51f
ydyd1115 <ydyd1115@gmail.com>
2022-12-15 20:23:49 +0900

    add_circleCI

--- config/database.php
+++ config/database.php
@@ -63,24 +63,19 @@ return [
             ]) : [],
         ],
 
-        'rese_test' => [
+        'rese' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
             'database' => 'rese_test',
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'username' => 'root',
+            'password' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
         ],
 
         'pgsql' => [
