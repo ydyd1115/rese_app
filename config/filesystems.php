edfb2cb10a276a3a5f07b80f82068ddcd5f90644
ydyd1115 <ydyd1115@gmail.com>
2022-12-19 15:28:20 +0900

    1219fixed

--- config/filesystems.php
+++ config/filesystems.php
@@ -14,7 +14,7 @@ return [
     */
 
     'default' => env('FILESYSTEM_DRIVER', 'local'),
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').':8000/storage',
             'visibility' => 'public',
         ],
 
